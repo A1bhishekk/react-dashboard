c8d4902064c8d0488deebbdc8137157ac975086a
Abhishek kumar <sahniak56@gmail.com>
2023-07-11 23:45:46 +0530

    chart

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = () => {
         <div className="user">
           <img
             src="https://images.pexels.com/photos/11038549/pexels-photo-11038549.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
-            alt=""
+            alt="profile"
           />
           <span>Rajnish</span>
         </div>
