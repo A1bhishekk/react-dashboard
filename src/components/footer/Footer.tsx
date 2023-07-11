cc3435329e27a9b3f9c2c5e70e468f1f6f7b9f24
Abhishek kumar <sahniak56@gmail.com>
2023-07-11 22:03:07 +0530

    dashboard

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Abhishek</span>
+      <span>© Technical Abhi Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
